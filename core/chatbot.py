8752614e2faba1df30275e12c2f547cfa50a2d6b
Manideep0905 <manideepjakkula9@gmail.com>
2026-02-06 20:51:39 +0530

    feat: use local model for the chatbot

--- core/chatbot.py
+++ core/chatbot.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 from core.memory import init_memory
 from utils.tools import get_tools
@@ -8,18 +8,26 @@ from utils.tools import get_tools
 load_dotenv()
 
 def build_agent():
-    llm = ChatOpenAI(
-        model="gpt-4o-mini-2024-07-18",
-        api_key=os.getenv("OPENAI_API_KEY")
+
+    # setup llm
+    llm = ChatOllama(
+        model="llama3.2:3b",
+        temperature=0
     )
 
+    # get tools and memory
     tools = get_tools()
     memory = init_memory()
 
+    # Tells the llm the role and operations which need to be performed.
+    system_message = "You are a helpful assistant. You have acces to a search tool. If the user asks for facts or current events, YOU MUST use the search tool. Do not guess."
+
+    # create the agent
     agent_executor = create_react_agent(
         model=llm,
         tools=tools,
-        checkpointer=memory
+        checkpointer=memory,
+        prompt=system_message
     )
 
     return agent_executor
